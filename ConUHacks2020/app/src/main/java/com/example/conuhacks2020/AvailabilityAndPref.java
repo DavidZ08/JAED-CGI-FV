5114b4ce87f6acabdc23069cfc49328769c8b6b8
DavidZ08 <dav.molina18@gmail.com>
2020-01-25 22:09:12 -0500

    Changes

--- ConUHacks2020/app/src/main/java/com/example/conuhacks2020/AvailabilityAndPref.java
+++ ConUHacks2020/app/src/main/java/com/example/conuhacks2020/AvailabilityAndPref.java
@@ -2,7 +2,9 @@ package com.example.conuhacks2020;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -14,6 +16,14 @@ public class AvailabilityAndPref extends AppCompatActivity {
         setContentView(R.layout.activity_availability_and_pref);
 
         Button nextBtn = (Button) findViewById(R.id.NextBtn);
+
+        nextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent toSwipingAct = new Intent(AvailabilityAndPref.this, SwipingActivity.class);
+               AvailabilityAndPref.this.startActivity(toSwipingAct);
+            }
+        });
 
         TextView monBegH = (TextView) findViewById(R.id.MonBegH);
         TextView monEndH = (TextView) findViewById(R.id.MonEndH);
--- ConUHacks2020/app/src/main/java/com/example/conuhacks2020/SwipingActivity.java
+++ ConUHacks2020/app/src/main/java/com/example/conuhacks2020/SwipingActivity.java
@@ -3,12 +3,99 @@ package com.example.conuhacks2020;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
+
+import com.lorentzos.flingswipe.SwipeFlingAdapterView;
+
+import java.util.ArrayList;
+
 
 public class SwipingActivity extends AppCompatActivity {
+
+    private ArrayList<String> al;
+    private ArrayAdapter<String> arrayAdapter;
+    private int i;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_swiping);
-    }
+
+
+
+        al = new ArrayList<>();
+        al.add("Volunt. Event 1");
+        al.add("Volunt. Event 2");
+        al.add("Volunt. Event 3");
+        al.add("Volunt. Event 4");
+        al.add("Volunt. Event 5");
+        al.add("Volunt. Event 6");
+        al.add("Volunt. Event 7");
+        al.add("Volunt. Event 8"); // you can add more events, as long as you add them to the array list
+
+        arrayAdapter = new ArrayAdapter<>(this, R.layout.item, R.id.helloText, al );
+
+        SwipeFlingAdapterView flingContainer = (SwipeFlingAdapterView) findViewById(R.id.frame);
+
+        flingContainer.setAdapter(arrayAdapter);
+        flingContainer.setFlingListener(new SwipeFlingAdapterView.onFlingListener() {
+            @Override
+            public void removeFirstObjectInAdapter() {
+                // this is the simplest way to delete an object from the Adapter (/AdapterView)
+                Log.d("LIST", "removed object!");
+                al.remove(0);
+                arrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onLeftCardExit(Object dataObject) {
+                //Do something on the left!
+                //You also have access to the original object.
+                //If you want to use it just cast it (String) dataObject
+                Toast.makeText(SwipingActivity.this,"Rejected",Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onRightCardExit(Object dataObject) {
+                Toast.makeText(SwipingActivity.this,"Accepted",Toast.LENGTH_SHORT).show();
+                // this is where you should create an intent with an "extra" (extra info about the event that will be passed on to the next Activity)
+            }
+
+            @Override
+            public void onAdapterAboutToEmpty(int itemsInAdapter) {
+                // Ask for more data here
+                al.add("XML ".concat(String.valueOf(i)));
+                arrayAdapter.notifyDataSetChanged();
+                Log.d("LIST", "notified");
+                i++;
+            }
+
+            @Override
+            public void onScroll(float scrollProgressPercent) {
+               // View view = flingContainer.getSelectedView();
+                //view.findViewById(R.id.item_swipe_right_indicator).setAlpha(scrollProgressPercent < 0 ? -scrollProgressPercent : 0);
+                //view.findViewById(R.id.item_swipe_left_indicator).setAlpha(scrollProgressPercent > 0 ? scrollProgressPercent : 0);
+            }
+        });
+
+
+
+
+
+
+
+
+
+}
 }
